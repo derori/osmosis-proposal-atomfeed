83465a202ec95e650435e1ef37ac64fc9185fb1a
derori <derori.derori@gmail.com>
2021-10-27 20:20:06 +0900

    Fixed Datetime parsing and feed sorting.

--- main.ts
+++ main.ts
@@ -15,24 +15,17 @@ const fetchProposals = (async () => {
     const url = 'https://lcd-osmosis.keplr.app/gov/proposals';
     const { data } = await axios.get(url);
     const ooo = data as Response;
-    console.dir(ooo);
-
-
-    ooo.result.sort((a, b) => {
-        return (a.id > b.id) ? -1 : 1;
-    });
-
+    ooo.result = ooo.result.sort((a, b) => b.id - a.id);
     for (const oo of ooo.result) {
         if (!oo.id) continue;
         feed.addItem({
-            title: `VotingEnd: ${moment(oo.voting_end_time).toISOString()} **${oo.content.value.title}`,
+            title: `VotingEnd: ${new Date(Date.parse(oo.voting_end_time)).toUTCString()} **${oo.content.value.title}`,
             link: `https://wallet.keplr.app/#/osmosis/governance?detailId=${oo.id}`,
-            date: moment(oo.submit_time).toDate(),
+            date: new Date(Date.parse(oo.submit_time)),
             description: oo.content.value.description,
             id: oo.id.toString(),
         });
 
-        // console.dir(`proposal_id:${oo.proposal_id}, date:${oo.voting_end_time}`);
     }
     interface Response {
         height: number,
